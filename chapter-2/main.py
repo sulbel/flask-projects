50579e696df0c89f6357c12d7231afe2ec07790d
sulbel <20748700+sulbel@users.noreply.github.com>
2021-05-23 15:16:52 -0500

    added code for sqlite database connection with sqlalchemy

---
+++ chapter-2/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import DevConfig
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+
+
+@app.route('/')
+def home():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
